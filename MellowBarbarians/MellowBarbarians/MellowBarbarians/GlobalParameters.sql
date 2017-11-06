cb357f6571aec7b557379bc1e512c9013808f59f
Evan Norsworthy <enorswo@gmail.com>
2017-11-06 16:46:32 -0500

    -Reduces the size of barbarian raids and attacks for medium difficulties (prince-emperor). -Reduces the max number of barb camps per civ. -Increases the minimum spawn distance from cities. -Decreases the minimum distance between barb camps. -Stops barbarians from becoming more aggressive after killing a unit.

---
+++ MellowBarbarians/MellowBarbarians/MellowBarbarians/GlobalParameters.sql
@@ -0,0 +1,31 @@
+-- Barbarians
+-- Author: Evan Norsworthy
+-- DateCreated: 11/3/2017 9:09:54 PM
+--------------------------------------------------------------
+--Max barb camps per Major Civilization, default value of 3
+UPDATE GlobalParameters SET Value = '2' WHERE Name = 'BARBARIAN_CAMP_MAX_PER_MAJOR_CIV';
+
+--Min spawn distance from cities, default value of 4
+UPDATE GlobalParameters SET Value = '6' WHERE Name = 'BARBARIAN_CAMP_MINIMUM_DISTANCE_CITY';
+	
+--Space Between Barbarian Camps, default value of 7
+UPDATE GlobalParameters SET Value = '4' WHERE Name = 'BARBARIAN_CAMP_MINIMUM_DISTANCE_ANOTHER_CAMP';
+
+--Stop barbarians from becoming more aggressive after killing a civ's unit
+UPDATE GlobalParameters SET Value = '0' WHERE Name = 'BARBARIAN_BOLDNESS_PER_KILL';
+
+--Reduce the number of units in barbarian raids of all types
+UPDATE BarbarianAttackForces
+SET NumMeleeUnits = '1', NumRangeUnits = '1', NumSiegeUnits = '0'
+WHERE AttackForceType = 'StandardRaid' OR AttackForceType = 'StandardAttack';
+UPDATE BarbarianAttackForces
+SET NumMeleeUnits = '1', NumRangeUnits = '1', NumSiegeUnits = '0'
+WHERE AttackForceType = 'CavalryRaid' OR AttackForceType = 'CavalryAttack';
+UPDATE BarbarianAttackForces
+SET NumMeleeUnits = '1', NumRangeUnits = '1', NumSiegeUnits = '0'
+WHERE AttackForceType = 'NavalRaid' OR AttackForceType = 'NavalAttack';
+
+--Increase the passive unit spawn rate of barbarian camps (default: 15 for melee and naval, 25 for cavalry)
+--UPDATE BarbarianTribes
+--SET TurnsToWarriorSpawn = '10'
+--WHERE TribeType = 'TRIBE_NAVAL' OR TribeType = 'TRIBE_CAVALRY' OR TribeType = 'TRIBE_MELEE';
