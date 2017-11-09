b020fa290fe2fef1d1f10ddd4caf31802c945c4b
Evan Norsworthy <enorswo@gmail.com>
2017-11-09 16:35:20 -0500

    Give cities an inherent 50 fortification

--- PeaceWar/PeaceWar/Buildings.sql
+++ PeaceWar/PeaceWar/Buildings.sql
@@ -2,6 +2,16 @@
 -- Author: Evan Norsworthy
 -- DateCreated: 10/27/2017 10:19:42 AM
 --------------------------------------------------------------
+--Make Medieval and Renaissance walls stronger and cheaper
+UPDATE Buildings
+SET Cost = Cost/2, PurchaseYield = 'YIELD_GOLD', OuterDefenseStrength = '5'
+WHERE BuildingType = 'BUILDING_WALLS' OR 'BUILDING_CASTLE' OR 'BUILDING_STAR_FORT';
+
+--Palace gives a large defensive bonus
+UPDATE Buildings
+SET OuterDefenseStrength = '50', OuterDefenseHitpoints = '50'
+WHERE BuildingType = 'BUILDING_PALACE';
+
 --Create a precursor to ancient walls
 INSERT INTO Types
 		(Type,								Kind)
@@ -13,17 +23,15 @@ INSERT INTO Buildings
 SELECT  'BUILDING_EPN_CITY_DEFENSES',	'LOC_BUILDING_EPN_CITY_DEFENSES_NAME',	'LOC_BUILDING_EPN_CITY_DEFENSES_DESCRIPTION',	0,							1,		NULL,		AdvisorType, TraitType, PrereqCivic, MaxPlayerInstances, MaxWorldInstances, Capital, PrereqDistrict, AdjacentDistrict, RequiresPlacement, RequiresRiver, OuterDefenseHitPoints, Housing, Entertainment, AdjacentResource, Coast, EnabledByReligion, AllowsHolyCity, PurchaseYield, MustPurchase, Maintenance, IsWonder, CitizenSlots, MustBeLake, MustNotBeLake, RegionalRange, AdjacentToMountain, ObsoleteEra, RequiresReligion, GrantFortification, DefenseModifier, InternalOnly, RequiresAdjacentRiver, Quote, QuoteAudio, MustBeAdjacentLand, AdjacentCapital, AdjacentImprovement, CityAdjacentTerrain
 FROM Buildings WHERE BuildingType = 'BUILDING_WALLS';
 
---Start each city out with city defenses
-INSERT INTO StartingBuildings
-		(Building,						Era,			District)
-VALUES	('BUILDING_EPN_CITY_DEFENSES',	'ERA_ANCIENT',	'DISTRICT_CITY_CENTER');
+--Hide the Castrum in the Civilopedia
+INSERT INTO CivilopediaPageExcludes (SectionId, PageId) VALUES ('BUILDINGS', 'BUILDING_EPN_CITY_DEFENSES');
 
---Make Medieval and Renaissance walls stronger and cheaper
-UPDATE Buildings
-SET Cost = Cost/2, PurchaseYield = 'YIELD_GOLD', OuterDefenseStrength = '5'
-WHERE BuildingType = 'BUILDING_WALLS' OR 'BUILDING_CASTLE' OR 'BUILDING_STAR_FORT';
-
---Palace gives a large defensive bonus
-UPDATE Buildings
-SET OuterDefenseStrength = '50', OuterDefenseHitpoints = '50'
-WHERE BuildingType = 'BUILDING_PALACE';
+--Make a castrum spawn in the city center of every new city
+INSERT INTO DistrictModifiers (DistrictType, ModifierId)
+VALUES ('DISTRICT_CITY_CENTER', 'BUILDING_CITY_DEFENSES_MODIFIER');
+--
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId)
+VALUES ('BUILDING_CITY_DEFENSES_MODIFIER', 'MODIFIER_SINGLE_CITY_GRANT_BUILDING_IN_CITY', 0, 1, NULL, NULL);
+--
+INSERT INTO ModifierArguments (ModifierId, Name, Type, Value, Extra, SecondExtra)
+VALUES ('BUILDING_CITY_DEFENSES_MODIFIER', 'BuildingType', 'ARGTYPE_IDENTITY', 'BUILDING_EPN_CITY_DEFENSES', NULL, NULL);
