1661c9698de3a5cdfe949481477fdb2a1f81d342
Evan Norsworthy <enorswo@gmail.com>
2017-11-09 23:18:59 -0500

    Text and documentation changes

--- CivilizationBalance/CivilizationBalance/Buildings.sql
+++ CivilizationBalance/CivilizationBalance/Buildings.sql
@@ -9,12 +9,12 @@ INSERT INTO Types
 		(Type,								Kind)
 VALUES	('BUILDING_EPN_HAPPINESS',		'KIND_BUILDING');
 
---Create a precursor to ancient walls
+--Create a dummy happiness building
 INSERT INTO Buildings	
 		(BuildingType,				Name,								Description,								Cost,	PrereqDistrict,			Entertainment,	MaxPlayerInstances)
 VALUES  ('BUILDING_EPN_HAPPINESS',	'LOC_BUILDING_EPN_HAPPINESS_NAME',	'LOC_BUILDING_EPN_HAPPINESS_DESCRIPTION',	1,		'DISTRICT_CITY_CENTER',	4,				4);
 
---Hide the Castrum in the Civilopedia
+--Hide the happiness building in the Civilopedia
 INSERT INTO CivilopediaPageExcludes (SectionId, PageId) VALUES ('BUILDINGS', 'BUILDING_EPN_HAPPINESS');
 
 --Make a happiness building spawn in the city center of the first four new cities
