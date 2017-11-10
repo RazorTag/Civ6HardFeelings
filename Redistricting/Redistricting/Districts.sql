baece4273436302767eb014aa78277ba8b0a0515
Evan Norsworthy <enorswo@gmail.com>
2017-11-10 15:59:42 -0500

    Changed district cost progression from game progression to the number that the player has built.

--- Redistricting/Redistricting/Districts.sql
+++ Redistricting/Redistricting/Districts.sql
@@ -2,8 +2,66 @@
 -- Author: enors
 -- DateCreated: 11/9/2017 1:36:24 PM
 --------------------------------------------------------------
---Make districts a constant production cost
-UPDATE Districts SET Cost = 2*Cost, CostProgressionModel = 'NO_COST_PROGRESSION';
+--Increase specialist yields
+UPDATE District_CitizenYieldChanges SET YieldChange = 3 WHERE (DistrictType = 'DISTRICT_HOLY_SITE' OR DistrictType = 'DISTRICT_LAVRA') AND YieldType = 'YIELD_FAITH';
+UPDATE District_CitizenYieldChanges SET YieldChange = 3 WHERE DistrictType = 'DISTRICT_CAMPUS' AND YieldType = 'YIELD_SCIENCE';
+UPDATE District_CitizenYieldChanges SET YieldChange = 2 WHERE DistrictType = 'DISTRICT_ENCAMPMENT' AND YieldType = 'YIELD_PRODUCTION';
+UPDATE District_CitizenYieldChanges SET YieldChange = 2 WHERE (DistrictType = 'DISTRICT_HARBOR' OR DistrictType = 'DISTRICT_ROYAL_NAVY_DOCKYARD') AND YieldType = 'YIELD_SCIENCE';
+UPDATE District_CitizenYieldChanges SET YieldChange = 6 WHERE DistrictType = 'DISTRICT_COMMERCIAL_HUB' AND YieldType = 'YIELD_GOLD';
+UPDATE District_CitizenYieldChanges SET YieldChange = 3 WHERE (DistrictType = 'DISTRICT_THEATER' OR DistrictType = 'DISTRICT_ACROPOLIS') AND YieldType = 'YIELD_CULTURE';
+UPDATE District_CitizenYieldChanges SET YieldChange = 3 WHERE (DistrictType = 'DISTRICT_INDUSTRIAL_ZONE' OR DistrictType = 'DISTRICT_HANSA') AND YieldType = 'YIELD_PRODUCTION';
 
---Reduce yields of occupied cities
+-- set other districts to cost more based on previous copies
+UPDATE Districts SET CostProgressionModel = 'COST_PROGRESSION_PREVIOUS_COPIES', CostProgressionParam1 = Cost/2
+WHERE
+  DistrictType = 'DISTRICT_AERODROME' OR
+  DistrictType = 'DISTRICT_CAMPUS' OR
+  DistrictType = 'DISTRICT_COMMERCIAL_HUB' OR
+  DistrictType = 'DISTRICT_ENCAMPMENT' OR
+  DistrictType = 'DISTRICT_ENTERTAINMENT_COMPLEX' OR
+  DistrictType = 'DISTRICT_HARBOR' OR
+  DistrictType = 'DISTRICT_HOLY_SITE' OR
+  DistrictType = 'DISTRICT_INDUSTRIAL_ZONE' OR
+  DistrictType = 'DISTRICT_THEATER' OR
+  DistrictType = 'DISTRICT_ACROPOLIS' OR
+  DistrictType = 'DISTRICT_LAVRA' OR
+  DistrictType = 'DISTRICT_ROYAL_NAVY_DOCKYARD' OR
+  DistrictType = 'DISTRICT_STREET_CARNIVAL' OR
+  DistrictType = 'DISTRICT_HANSA';
 
+--Tier 1 districts
+UPDATE Districts SET Cost = '60' WHERE
+  DistrictType = 'DISTRICT_CAMPUS' OR
+  DistrictType = 'DISTRICT_ENCAMPMENT' OR
+  DistrictType = 'DISTRICT_HOLY_SITE';
+
+--Tier 2 districts
+UPDATE Districts SET Cost = '90' WHERE
+  DistrictType = 'DISTRICT_COMMERCIAL_HUB' OR
+  DistrictType = 'DISTRICT_ENTERTAINMENT_COMPLEX' OR
+  DistrictType = 'DISTRICT_HARBOR' OR
+  DistrictType = 'DISTRICT_THEATER';
+
+--Tier 3 districts
+UPDATE Districts SET Cost = '120' WHERE
+  DistrictType = 'DISTRICT_INDUSTRIAL_ZONE';
+
+--Tier 4 districts
+UPDATE Districts SET Cost = '210' WHERE
+  DistrictType = 'DISTRICT_AERODROME';
+
+--Set unique districts to half the cost of the district that they replace
+UPDATE Districts
+	SET Cost = 
+	(
+		SELECT .5*Cost
+		FROM Districts AS d
+		JOIN DistrictReplaces
+		ON d.DistrictType = DistrictReplaces.ReplacesDistrictType
+		WHERE Districts.DistrictType = DistrictReplaces.CivUniqueDistrictType
+	)
+	WHERE DistrictType IN
+	(
+		SELECT DISTINCT CivUniqueDistrictType
+		FROM DistrictReplaces
+	);
