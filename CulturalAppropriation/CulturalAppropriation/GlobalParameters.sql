ce1efb25e3217a963d4513dab2089ea01c1f6cca
Evan Norsworthy <enorswo@gmail.com>
2017-11-13 16:44:41 -0500

    Merged religion enhancements with Cultural Appropriation

--- CulturalAppropriation/CulturalAppropriation/GlobalParameters.sql
+++ CulturalAppropriation/CulturalAppropriation/GlobalParameters.sql
@@ -2,6 +2,21 @@
 -- Author: enors
 -- DateCreated: 11/10/2017 1:08:08 PM
 --------------------------------------------------------------
+--Religion
+--------------------------------------------------------------
+--Enable unlocking of governments with faith
+--UPDATE GlobalParameters SET Value = 'true' WHERE Name = 'GOVERNMENT_UNLOCK_WITH_FAITH';
+
+--Faster natural spread for religion (default: 1)
+UPDATE GlobalParameters SET Value = '10' WHERE Name = 'RELIGION_SPREAD_ADJACENT_PER_TURN_PRESSURE';
+
+--Condemning religious units does not remove their religion from nearby cities
+UPDATE GlobalParameters SET Value = '0' WHERE Name = 'RELIGION_SPREAD_UNIT_CAPTURE';
+
+
+--------------------------------------------------------------
+--Culture
+--------------------------------------------------------------
 --Prevent the space race from hard countering tourism
 UPDATE ModifierArguments SET Value = '1' WHERE ModifierId = 'PROJECT_COMPLETION_GRANT_CULTURE_BASED_ON_SCIENCE_RATE' and Name = 'Multiplier';
 
