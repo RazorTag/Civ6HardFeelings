6782ff0eda1475026a943794a3769dcc84bc95d8
Evan Norsworthy <enorswo@gmail.com>
2017-11-13 16:36:59 -0500

    Castrum adds 3 happiness and is limited to 3 cities. Increased Catrum hitpoints from 25 to 50. Increased Catrum defense bonus from 25 to 50.

--- PeaceWar/PeaceWar/Buildings.sql
+++ PeaceWar/PeaceWar/Buildings.sql
@@ -13,7 +13,7 @@ WHERE BuildingType = 'BUILDING_CASTLE' OR BuildingType = 'BUILDING_STAR_FORT';
 
 --Palace gives a large defensive bonus
 UPDATE Buildings
-SET OuterDefenseStrength = '50', OuterDefenseHitpoints = '50'
+SET OuterDefenseStrength = '50', OuterDefenseHitpoints = '100'
 WHERE BuildingType = 'BUILDING_PALACE';
 
 
@@ -25,10 +25,10 @@ INSERT INTO Types
 		(Type,								Kind)
 VALUES	('BUILDING_EPN_CITY_DEFENSES',		'KIND_BUILDING');
 
---Create a precursor to ancient walls
+--Create a precursor to ancient walls (Castrum)
 INSERT INTO Buildings	
-		(BuildingType,					Name,									Description,									OuterDefenseStrength,		Cost,	PrereqTech, MaxPlayerInstances, MustPurchase,	PurchaseYield,	AdvisorType, TraitType, PrereqCivic, MaxWorldInstances, Capital, PrereqDistrict, AdjacentDistrict, RequiresPlacement, RequiresRiver, OuterDefenseHitPoints, Housing, Entertainment, AdjacentResource, Coast, EnabledByReligion, AllowsHolyCity, Maintenance, IsWonder, CitizenSlots, MustBeLake, MustNotBeLake, RegionalRange, AdjacentToMountain, ObsoleteEra, RequiresReligion, GrantFortification, DefenseModifier, InternalOnly, RequiresAdjacentRiver, Quote, QuoteAudio, MustBeAdjacentLand, AdjacentCapital, AdjacentImprovement, CityAdjacentTerrain)
-SELECT  'BUILDING_EPN_CITY_DEFENSES',	'LOC_BUILDING_EPN_CITY_DEFENSES_NAME',	'LOC_BUILDING_EPN_CITY_DEFENSES_DESCRIPTION',	25,							1,		NULL,		4,					1,				NULL,			AdvisorType, TraitType, PrereqCivic, MaxWorldInstances, Capital, PrereqDistrict, AdjacentDistrict, RequiresPlacement, RequiresRiver, OuterDefenseHitPoints, Housing, Entertainment, AdjacentResource, Coast, EnabledByReligion, AllowsHolyCity, Maintenance, IsWonder, CitizenSlots, MustBeLake, MustNotBeLake, RegionalRange, AdjacentToMountain, ObsoleteEra, RequiresReligion, GrantFortification, DefenseModifier, InternalOnly, RequiresAdjacentRiver, Quote, QuoteAudio, MustBeAdjacentLand, AdjacentCapital, AdjacentImprovement, CityAdjacentTerrain
+		(BuildingType,					Name,									Description,									OuterDefenseStrength,	OuterDefenseHitPoints,	Entertainment,		Cost,	PrereqTech, MaxPlayerInstances, MustPurchase,	PurchaseYield,	AdvisorType, TraitType, PrereqCivic, MaxWorldInstances, Capital, PrereqDistrict, AdjacentDistrict, RequiresPlacement, RequiresRiver, Housing, AdjacentResource, Coast, EnabledByReligion, AllowsHolyCity, Maintenance, IsWonder, CitizenSlots, MustBeLake, MustNotBeLake, RegionalRange, AdjacentToMountain, ObsoleteEra, RequiresReligion, GrantFortification, DefenseModifier, InternalOnly, RequiresAdjacentRiver, Quote, QuoteAudio, MustBeAdjacentLand, AdjacentCapital, AdjacentImprovement, CityAdjacentTerrain)
+SELECT  'BUILDING_EPN_CITY_DEFENSES',	'LOC_BUILDING_EPN_CITY_DEFENSES_NAME',	'LOC_BUILDING_EPN_CITY_DEFENSES_DESCRIPTION',	50,						100,					3					1,		NULL,		3,					1,				NULL,			AdvisorType, TraitType, PrereqCivic, MaxWorldInstances, Capital, PrereqDistrict, AdjacentDistrict, RequiresPlacement, RequiresRiver, Housing, AdjacentResource, Coast, EnabledByReligion, AllowsHolyCity, Maintenance, IsWonder, CitizenSlots, MustBeLake, MustNotBeLake, RegionalRange, AdjacentToMountain, ObsoleteEra, RequiresReligion, GrantFortification, DefenseModifier, InternalOnly, RequiresAdjacentRiver, Quote, QuoteAudio, MustBeAdjacentLand, AdjacentCapital, AdjacentImprovement, CityAdjacentTerrain
 FROM Buildings WHERE BuildingType = 'BUILDING_WALLS';
 
 --Hide the Castrum in the Civilopedia
